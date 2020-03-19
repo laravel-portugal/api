f16b1a26ae3b50fa824376077cdeb2d07235e7e3
José Postiga <jpostiga@infraspeak.com>
2020-03-19 18:51:17 +0000

    style(Tags): minor coding style fixes

--- domains/Tags/Database/Factories/TagFactory.php
+++ domains/Tags/Database/Factories/TagFactory.php
@@ -2,8 +2,9 @@
 
 use Carbon\Carbon;
 use Domains\Tags\Models\Tag;
+use Illuminate\Database\Eloquent\Factory;
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+/** @var Factory $factory */
 $factory->define(Tag::class, static fn (\Faker\Generator $faker) => [
     'name' => $faker->name,
     'created_at' => Carbon::now(),
