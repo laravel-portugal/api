a6a1c6f0264bbb79b5005e610260a32a1f2e7246
Tiago Fernandes <tiagoff@gmail.com>
2020-10-04 21:59:36 +0100

    remove cached views

--- storage/framework/views/32fffd8e0b255f12747fd46d8d36380158184464.php
+++
@@ -1,2 +0,0 @@
-[<?php echo e($slot); ?>](<?php echo e($url); ?>)
-<?php /**PATH /Users/tiago/DATA/OSS/laravel-portugal/api/vendor/illuminate/mail/resources/views/text/header.blade.php ENDPATH**/ ?>
