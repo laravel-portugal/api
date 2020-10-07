09ad18e3ee05bb632dc146ba018c457ed8bc3a74
adevr <alex.cst.reis@gmail.com>
2020-10-07 22:23:56 +0100

    feat(Tags): add support to timestamps w/timezones
    - drop regular timestamps columns
    - create new ones with timestampsTz

---
+++ domains/Tags/Database/Migrations/2020_10_07_160608_alter_timestamps_columns_to_tags_table.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterTimestampsColumnsToTagsTable extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('tags', function (Blueprint $table) {
+            $table->dropColumn(["created_at", "updated_at", "deleted_at"]);
+        });
+
+        Schema::table('tags', function (Blueprint $table) {
+            $table->timestampsTz();
+            $table->timestampTz("deleted_at")->nullable();
+        });
+    }
+}
