aa2fd7fce8d57d9ede2af758e1178973e243a530
adevr <alex.cst.reis@gmail.com>
2020-10-10 17:26:14 +0100

    fix(Links): add missing timestamp (approved_at)

--- domains/Links/Database/Migrations/2020_10_10_152640_alter_timestamps_columns_to_links_table.php
+++ domains/Links/Database/Migrations/2020_10_10_152640_alter_timestamps_columns_to_links_table.php
@@ -9,12 +9,14 @@ class AlterTimestampsColumnsToLinksTable extends Migration
     public function up(): void
     {
         Schema::table('links', function (Blueprint $table) {
-            $table->dropColumn(["created_at", "updated_at", "deleted_at"]);
+            $table->dropColumn(["created_at", "updated_at", "approved_at", "deleted_at"]);
         });
 
         Schema::table('links', function (Blueprint $table) {
             $table->timestampsTz();
             $table->timestampTz("deleted_at")->nullable();
+            $table->timestampTz("approved_at")->nullable();
         });
     }
 }
+    
