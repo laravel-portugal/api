a66b3eea1711eab67d65bcf645e8124734fa7cd3
Daniel Maciel <danielmaciel@surge.pt>
2020-10-15 00:09:21 +0100

    - Implement Authenticatable

--- domains/Accounts/Models/User.php
+++ domains/Accounts/Models/User.php
@@ -8,14 +8,22 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Lumen\Auth\Authorizable;
 use Laravel\Passport\HasApiTokens;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Auth\Authenticatable as AuthenticableTrait;
 
-class User extends Model
+
+class User extends Model implements Authenticatable
 {
     use MustVerifyEmail;
     use Notifiable;
     use SoftDeletes;
     use Authorizable;
     use HasApiTokens;
+    use AuthenticableTrait;
+
+    protected $hidden = [
+        'password', 'trusted', 'email_verified_at'
+    ];
 
     protected $casts = [
         'email_verified_at' => 'date',
