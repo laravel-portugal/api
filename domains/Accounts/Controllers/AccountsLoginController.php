b730fd11e1210e1a78ff8a78da1824a1c3b25af3
Daniel Maciel <danielmaciel@surge.pt>
2020-10-14 23:32:08 +0100

    - Init

--- domains/Accounts/Controllers/AccountsLoginController.php
+++ domains/Accounts/Controllers/AccountsLoginController.php
@@ -21,8 +21,8 @@ class AccountsLoginController extends Controller
         $user = User::where('email', $request->email)->first();
         if ($user) {
             if (Hash::check($request->password, $user->password)) {
-                $token    = $user->createToken('Laravel Password Grant Client')->accessToken;
-                $response = ['token' => $token];
+                $token    = $user->createToken($user->email . '-'. Request()->ip())->accessToken;
+                $response = ['access_token' => $token];
                 return response($response, 200);
             } else {
                 $response = ["message" => "Password mismatch"];
