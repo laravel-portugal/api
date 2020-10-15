1c9e46943f9ed9e5d6268c87cf232de526a7d18c
Daniel Maciel <danielmaciel@surge.pt>
2020-10-15 14:21:35 +0100

    -  Init

---
+++ domains/Accounts/Tests/Feature/AccountsLoginTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Domains\Accounts\Tests\Feature;
+
+use Domains\Accounts\Database\Factories\UserFactory;
+use Domains\Accounts\Models\User;
+use Faker\Factory;
+use Faker\Generator;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class AccountsLoginTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    protected User $user;
+    protected Generator $faker;
+
+    protected function setUp(): void {
+        parent::setUp();
+
+        $this->user  = UserFactory::new(['password' => Hash::make('greatpassword')])->create();
+        $this->faker = Factory::create();
+    }
+
+    /** @test */
+    public function it_fails_to_login_on_validation_errors(): void {
+
+        $response = $this->post(route('accounts.login'), [
+            'email' => $this->faker->safeEmail
+        ]);
+
+        $response->assertResponseStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+
+    }
+
+    /** @test */
+    public function guest_fail_login_with_not_exist_user(): void {
+
+        $response = $this->post(route('accounts.login'), [
+            'email'    => $this->faker->safeEmail,
+            'password' => $this->faker->password]);
+
+        $response->assertResponseStatus(Response::HTTP_NOT_FOUND);
+    }
+
+    /** @test */
+    public function guest_fail_login_with_wrong_credencial(): void {
+
+        $response = $this->post(route('accounts.login'), [
+            'email'    => $this->user->email,
+            'password' => $this->faker->password
+        ]);
+
+        $response->assertResponseStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+    /** @test */
+    public function guest_blocked_for_many_attempts(): void {
+
+        $this->markTestIncomplete();
+    }
+
+    /** @test */
+    public function guest_can_make_login_with_correct_credencial(): void {
+
+        $response = $this->post(route('accounts.login'), [
+            'email'    => $this->user->email,
+            'password' => 'greatpassword'
+        ]);
+        $response->assertResponseStatus(Response::HTTP_OK);
+    }
+
+}
