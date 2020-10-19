f38b5a376e239a4e04b17172f548fa436a5dec6b
Daniel Maciel <danielmaciel@surge.pt>
2020-10-19 11:04:56 +0100

    - Making change request

--- domains/Accounts/Models/User.php
+++ domains/Accounts/Models/User.php
@@ -27,7 +27,7 @@ class User extends Model implements AuthenticatableContract, JWTSubject
         'email_verified_at' => 'date',
     ];
 
-    public function getJWTIdentifier(): array
+    public function getJWTIdentifier(): int
     {
         return $this->getKey();
     }
